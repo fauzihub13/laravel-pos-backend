5b297aaa5f688df38effcffa93f58e7a960fe91c
fauzihub13 <fauziadisaputra3@gmail.com>
2024-06-24 18:55:54 +0700

    Initialize Project

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('pages.auth.login');
+});
+
+Route::middleware(['auth'])->group(function(){
+
+    Route::get('home', function (){
+        return view('pages.dashboard');
+    })->name('home');
+
+});
