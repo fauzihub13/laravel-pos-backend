aeae7e2e3419705aff2e5c6ac739f1c7f88deb38
fauzihub13 <fauziadisaputra3@gmail.com>
2024-06-24 21:26:07 +0700

    Auth, CRUD User

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,4 +13,7 @@ Route::middleware(['auth'])->group(function(){
         return view('pages.dashboard');
     })->name('home');
 
+    Route::resource('users', UserController::class);
+
 });
+
